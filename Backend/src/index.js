a46d198579058d3a260433afe507b1c4bef7f0b4
Kirubel Daniel <kirud87@gmail.com>
2025-07-22 03:14:12 +0300

    Add fix for Express v5 wildcard route3

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -26,13 +26,12 @@ app.use(
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*splat", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("/*splat", (req, res) => {
+   res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+});
 
 
 server.listen(PORT, () => {
